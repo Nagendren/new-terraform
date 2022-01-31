07f61f557cd75b74f0fde486bac4bba078d8095f
Nagendren <54283806+Nagendren@users.noreply.github.com>
2022-01-31 15:39:49 +0530

    Create dynamodb.tf
    Adding dynamodb.tf

---
+++ 04-remote-state/dynamodb.tf
@@ -0,0 +1,14 @@
+resource "aws_dynamodb_table" "terraform-lock" {
+    name           = "terraform_state"
+    read_capacity  = 5
+    write_capacity = 5
+    hash_key       = "LockID"
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+    tags = {
+        "Name" = "DynamoDB Terraform State Lock Table"
+    }
+}
+#The primary key to be used to lock the state in dynamoDB must be called LockID and must be a “string” type (S).
